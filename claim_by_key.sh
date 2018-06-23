37b922865c77f373b835adb3f52325f40eec4299
zhanghao <zhanghao@eosbeijing.one>
2018-06-23 19:37:42 +0800

    bug fix: should wait before unlock/create wallet

--- claim_by_key.sh
+++ claim_by_key.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# name to claim and private key
-BP=
+# bp name
+BP=eosbeijingbp
+# private key
 PRIVATE_KEY=
+# private key permission
+PERMISSION=active
 
 # env
 NODEOS_BIN_DIR='/eos/build/programs'
@@ -10,21 +13,21 @@ WALLET_HOST='127.0.0.1:3000'
 NODE_HOST='127.0.0.1:8080'
 CLEOS="$NODEOS_BIN_DIR/cleos/cleos -u http://$NODE_HOST --wallet-url http://$WALLET_HOST"
 
-# step 1: stop and remove current wallet
-rm -rf ~/eosio-wallet/*
-
-# step 2: create new wallet
-mkdir -p ~/eosio-wallet
-WALLET_RESULT=`$CLEOS wallet create`
-$CLEOS wallet import $PRIVATE_KEY
-
-# step 4: wait and claim rewards
+# step 1: wait until 24 hours from last claim
 last_claim_time=`$CLEOS get table eosio eosio producers -l 1000 | jq -r '.rows[] | select(.owner == "'$BP'") | .last_claim_time'`
 now=`date +%s%N`
 diff=`expr $last_claim_time / 1000000 + 24 \* 3600 - $now / 1000000000 + 1`
 echo "wait for ${diff}s"
 sleep $diff
-$CLEOS system claimrewards $BP
+
+# step 2: create new wallet and import key
+mkdir -p ~/eosio-wallet
+rm -rf ~/eosio-wallet/*
+WALLET_RESULT=`$CLEOS wallet create`
+$CLEOS wallet import $PRIVATE_KEY
+
+# step 3: claim rewards
+$CLEOS system claimrewards $BP -p $BP@$PERMISSION
 echo 'claimed at ' `date`
 
 # step 5: clean
--- claim_by_wallet_pw.sh
+++ claim_by_wallet_pw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # name to claim and wallet password
-BP=
+BP=eosbeijingbp
 WALLET_PW=
 
 # env
@@ -10,16 +10,16 @@ WALLET_HOST='127.0.0.1:3000'
 NODE_HOST='127.0.0.1:8080'
 CLEOS="$NODEOS_BIN_DIR/cleos/cleos -u http://$NODE_HOST --wallet-url http://$WALLET_HOST"
 
-# step 1: unlock wallet
-$CLEOS wallet open
-$CLEOS wallet unlock --password $WALLET_PW
-
-# step 2: wait and claim rewards
+# step 1: wait until 24 hours from last claim
 last_claim_time=`$CLEOS get table eosio eosio producers -l 1000 | jq -r '.rows[] | select(.owner == "'$BP'") | .last_claim_time'`
 now=`date +%s%N`
 diff=`expr $last_claim_time / 1000000 + 24 \* 3600 - $now / 1000000000 + 1`
 echo "wait for ${diff}s"
 sleep $diff
+
+# step 2: unlock wallet and claim
+$CLEOS wallet open
+$CLEOS wallet unlock --password $WALLET_PW
 $CLEOS system claimrewards $BP
 echo 'claimed at ' `date`
 
